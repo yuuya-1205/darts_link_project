a1e7e3640d246949f712a8dcf5e4976a628b82c8
奥野領太 <>
2023-03-25 21:12:12 +0900

    UIの軽微なリファクタリング

--- lib/views/darts_function_page/round_robin/round_robin_rank_list_page.dart
+++ lib/views/darts_function_page/round_robin/round_robin_rank_list_page.dart
@@ -1,7 +1,10 @@
+import 'package:darts_link_project/components/robin_text.dart';
 import 'package:darts_link_project/models/round_robin.dart';
 import 'package:darts_link_project/models/team.dart';
 import 'package:darts_link_project/repositories/team_repository.dart';
 import 'package:flutter/material.dart';
+
+import 'round_robin_card.dart';
 
 class RoundRobinRankListPage extends StatefulWidget {
   const RoundRobinRankListPage({super.key, required this.roundRobin});
@@ -15,46 +18,74 @@ class RoundRobinRankListPage extends StatefulWidget {
 class _RoundRobinRankListPageState extends State<RoundRobinRankListPage> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<Team>>(
-        stream: TeamRepository.teamStream(roundRobinId: widget.roundRobin.id),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.active) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (!snapshot.hasData) {
-            return Container();
-          }
-          final teams = snapshot.data;
-          if (teams!.isEmpty) {
-            return const Center(
-              child: Text('まだ、投稿がありません'),
-            );
-          }
-
-          return GridView.count(
-            //ここにチーム数を表示する teams.lenght
-            // １列に表示するアイテムの数を指定
-            crossAxisCount: teams.length,
-            // スクロールする向きを指定
-            scrollDirection: Axis.vertical,
-            // 200個アイテムを生成
-            //チーム数＊チーム数でやる。
-            children: List.generate(teams.length * teams.length, (index) {
-              return Container(
-                padding: const EdgeInsets.all(5.0),
-                width: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.red),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Container(
-                  child: const Text("Containerの角丸枠線"),
-                ),
+    return Column(
+      children: [
+        const SizedBox(
+          height: 12,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            RobinText(
+              width: 114,
+              text: '順位',
+            ),
+            RobinText(
+              width: 170,
+              text: 'チーム',
+            ),
+            RobinText(
+              width: 65,
+              text: '勝ち数',
+            ),
+            RobinText(
+              width: 65,
+              text: '負け数',
+            ),
+          ],
+        ),
+        Row(
+          children: [],
+        ),
+        StreamBuilder<List<Team>>(
+          stream: TeamRepository.teamStream(roundRobinId: widget.roundRobin.id),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.active) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            }),
-          );
-        });
+            }
+            if (!snapshot.hasData) {
+              return Container();
+            }
+            final teams = snapshot.data;
+            if (teams!.isEmpty) {
+              return const Center(
+                child: Text('まだ、投稿がありません'),
+              );
+            }
+            teams.forEach((element) {});
+
+            teams.sort(
+              (a, b) => b.totalWinCount.compareTo(a.totalWinCount),
+            );
+
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: teams.length,
+                  itemBuilder: (context, index) {
+                    final team = teams[index];
+                    return RoundRobinCard(
+                      team: team,
+                      teamCount: index + 1,
+                    );
+                  }),
+            );
+          },
+        ),
+      ],
+    );
   }
 }
