ecd5d812a22e33da14111db8518810ba8175cc23
奥野領太 <>
2023-04-08 16:57:08 +0900

    primaryColorを定数化

--- lib/theme_data.dart
+++ lib/theme_data.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class OriginalTheme {
   static final themeData = ThemeData(
-      primaryColor: const Color.fromRGBO(247, 63, 150, 1),
+      primaryColor: primary,
       disabledColor: const Color.fromRGBO(176, 176, 176, 1),
       dividerColor: const Color.fromRGBO(240, 242, 247, 1));
+
+  static const primary = Color.fromRGBO(247, 63, 150, 1);
 }
