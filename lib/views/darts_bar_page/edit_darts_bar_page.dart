4a8098f24a5d6d4ffaa92c0d71b3812086e3afa8
牛濱　雄也 <wheel.of.connection@gmail.com>
2023-04-20 20:59:47 +0900

    ダーツバー機能修正

---
+++ lib/views/darts_bar_page/edit_darts_bar_page.dart
@@ -0,0 +1,18 @@
+import 'package:darts_link_project/models/app_user.dart';
+import 'package:flutter/material.dart';
+
+class EditDartsBarPage extends StatefulWidget {
+  const EditDartsBarPage({super.key, required this.storeOwner});
+
+  final StoreOwner storeOwner;
+
+  @override
+  State<EditDartsBarPage> createState() => _EditDartsBarPageState();
+}
+
+class _EditDartsBarPageState extends State<EditDartsBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
